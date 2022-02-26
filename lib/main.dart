948c0edebf4f68a9b7d9603564c5383023c84090
yonathan <yonycherkos@gmail.com>
2022-02-26 14:45:47 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram Clone',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Instagram Clone'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor = Color.fromRGBO(0, 0, 0, 1);
+  static const searchColor = Color.fromRGBO(38, 38, 38, 1);
+  static const blueColor = Color.fromRGBO(0, 149, 246, 1);
+  static const primaryColor = Colors.white;
+  static const secondaryColor = Colors.grey;
+}
